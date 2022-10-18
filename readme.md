a5d0d037fad5036455e794d1270932619cdc598b
morka17 <joshuamorka19@gmail.com>
2022-10-18 14:50:04 -0700

    create the TcpListener, accept  the socket connection, reading and writing to the client

---
+++ readme.md
@@ -0,0 +1,4 @@
+
+# Chat server 
+
+This chat server basically allow multiple TCP Clients to connect up to the chat server, When one client send a message, the message will be broadcast to all of the different clients that are connected.  
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,44 @@
-fn main() {
-    println!("Hello, world!");
+use tokio::{
+    io::{AsyncBufReadExt, AsyncReadExt, AsyncWriteExt, BufReader},
+    net::TcpListener,
+    sync::broadcast 
+};
+
+
+
+#[tokio::main]
+async fn main() {
+    let listener = TcpListener::bind("localhost:8080").await.unwrap();
+
+    let (tx, rx) = broadcast::channel::<String>(10);
+
+  
+
+    loop {
+        let (mut socket, _addr) = listener.accept().await.unwrap();
+
+        let tx = tx.clone();
+
+        tokio::spawn(async move {
+            let (reader, mut writer) = socket.split();
+
+            let mut reader = BufReader::new(reader);
+
+            let mut line = String::new();
+
+            loop {
+                let bytes_read = reader.read_line(&mut line).await.unwrap(); // read byte for the accepted TcpStream
+
+                if bytes_read == 0 {
+                    break;
+                }
+
+                tx.send(line.clone()).unwrap();
+
+
+                writer.write_all(line.as_bytes()).await.unwrap();
+                line.clear()
+            }
+        });
+    }
 }
